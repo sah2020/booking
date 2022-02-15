b1b75e451eab5aa3f0783b7cea363d00b6daddfa
sah2020 <akmaljonsamandarov@gmail.com>
2022-02-15 17:48:11 +0500

    responseItem changed

--- hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/dto/ResponseItem.java
+++ hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/dto/ResponseItem.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 @Getter
@@ -14,6 +15,7 @@ public class ResponseItem implements Serializable {
     private String message;
     private boolean success;
     private Object object;
+    private HttpStatus httpStatusCode = HttpStatus.OK;
 
     public ResponseItem(String message, boolean success) {
         this.message = message;
