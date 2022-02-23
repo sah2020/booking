f64238102ebf3c392b3c39ec43cf761623a8c79a
sah2020 <akmaljonsamandarov@gmail.com>
2022-02-23 22:06:06 +0500

    request sent to backend from bot

--- telegram-bot/src/main/java/com/exadel/telegrambot/TelegramBotApplication.java
+++ telegram-bot/src/main/java/com/exadel/telegrambot/TelegramBotApplication.java
@@ -1,22 +1,25 @@
 package com.exadel.telegrambot;
 
+import com.exadel.telegrambot.dto.LoginDTO;
+import com.exadel.telegrambot.dto.ResponseItem;
+import com.exadel.telegrambot.dto.UserBasicResTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.kshashov.telegram.api.MessageType;
 import com.github.kshashov.telegram.api.TelegramMvcController;
-import com.github.kshashov.telegram.api.TelegramRequest;
 import com.github.kshashov.telegram.api.bind.annotation.BotController;
-import com.github.kshashov.telegram.api.bind.annotation.BotPathVariable;
 import com.github.kshashov.telegram.api.bind.annotation.BotRequest;
-import com.github.kshashov.telegram.api.bind.annotation.request.MessageRequest;
-import com.pengrad.telegrambot.Callback;
+import com.google.gson.Gson;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.BaseResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+
 
 @BotController
 @SpringBootApplication
@@ -32,11 +35,39 @@ public class TelegramBotApplication implements TelegramMvcController {
 
     @BotRequest(value = "/start", type = {MessageType.CALLBACK_QUERY, MessageType.MESSAGE})
     public BaseRequest start(User user, Chat chat) {
-        return new SendMessage(chat.id(), "Hello, " + user.firstName() + "!\n" +"Your telegram chat ID: "+ chat.id());
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername(chat.id().toString());
+        loginDTO.setPassword("password");
+
+        final String apiUrl = "http://localhost:8080/api/login";
+        final RestTemplate restTemplate = new RestTemplate();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        String requestBody = toJSON(loginDTO);
+        final HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        final String response = restTemplate.postForObject(apiUrl, request, String.class);
+        Gson gson = new Gson();
+        System.out.println(response);
+        ResponseItem responseItem = new ResponseItem();
+        responseItem.setData(new UserBasicResTO());
+        responseItem = gson.fromJson(response, ResponseItem.class);
+        return new SendMessage(chat.id(), "Response Received Successfully\n" + responseItem.getData().toString());
     }
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class);
+    }
+
+    public static String toJSON(Object object) {
+        if (object == null) {
+            return null;
+        }
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 }
 
---
+++ telegram-bot/src/main/java/com/exadel/telegrambot/dto/ResponseItem.java
@@ -0,0 +1,27 @@
+package com.exadel.telegrambot.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseItem implements Serializable {
+    private String message;
+    private int statusCode = 200;
+    private Object data;
+
+    public ResponseItem(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+}
+
