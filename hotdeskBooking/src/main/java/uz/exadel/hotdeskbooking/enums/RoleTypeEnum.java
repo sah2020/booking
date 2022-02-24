52b947b27f8504cef73298079c4900a26f88a4dd
user <m.muhammadsodiq777@gmail.com>
2022-02-24 09:56:43 +0500

    n

--- hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/enums/RoleTypeEnum.java
+++
@@ -1,8 +0,0 @@
-package uz.exadel.hotdeskbooking.enums;
-
-public enum RoleTypeEnum {
-    ROLE_COMMON_USER,
-    ROLE_MAP_EDITOR,
-    ROLE_ADMIN,
-    ROLE_MANAGER;
-}
--- hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.exadel.hotdeskbooking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.exadel.hotdeskbooking.domain.Role;
-import uz.exadel.hotdeskbooking.enums.RoleTypeEnum;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByRoleType(RoleTypeEnum roleType);
-}
