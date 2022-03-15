f7e552e8133c5b37e9457bed82c9207f053b7428
sah2020 <akmaljonsamandarov@gmail.com>
2022-03-16 00:22:48 +0500

    swagger added

--- hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/HotdeskBookingApplication.java
+++ hotdeskBooking/src/main/java/uz/exadel/hotdeskbooking/HotdeskBookingApplication.java
@@ -3,10 +3,12 @@ package uz.exadel.hotdeskbooking;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableSwagger2
 public class HotdeskBookingApplication {
 
     public static void main(String[] args) {
